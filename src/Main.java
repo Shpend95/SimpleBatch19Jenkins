c40fab7e35a01d3d1a94dead9ab04617e189c23f
Shpend95 <shpendpllana5@gmail.com>
2024-07-10 21:29:50 -0400

    new code added

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,12 @@ public class Main {
     public static void main(String[] args) throws InterruptedException {
 
 
-        for (int i = 0; i <10; i++) {
-            System.out.println("Batch19");
+        for (int i = 1; i <= 19; i++) {
+            if (i < 19) {
+                System.out.println("Batch " + i + " is great");
+            } else {
+                System.out.println("Batch 19 is the best");
+            }
             Thread.sleep(2000);
         }
     }
