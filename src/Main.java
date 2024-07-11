2356dbe9af4d47939b5390bdff9fbfb3381f85f8
Shpend95 <shpendpllana5@gmail.com>
2024-07-10 21:21:11 -0400

    new code added

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws InterruptedException {
+
+
+        for (int i = 0; i <10; i++) {
+            System.out.println("Batch19");
+            Thread.sleep(2000);
+        }
     }
 }
